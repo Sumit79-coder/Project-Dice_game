cdf0d00a2ce3e842e74c50b48e379073d239c614
Sumit Gupta <69911115+Sumit79-coder@users.noreply.github.com>
2022-06-22 22:27:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 randomNumber1 = Math.floor(Math.random() * 6 + 1)
 randomNumber2 = Math.floor(Math.random() * 6 + 1)
 
-num = console.log(randomNumber1)
-num = console.log(randomNumber2)
 
-console.log(document.getElementById("img1").src = "/images/dice"+randomNumber1+".png")
-console.log(document.getElementById("img2").src = "/images/dice"+randomNumber2+".png")
+document.getElementById("img1").src = "/images/dice"+randomNumber1+".png"
+document.getElementById("img2").src = "/images/dice"+randomNumber2+".png"
 
 if (randomNumber1>randomNumber2){
     console.log(document.querySelector("h1").textContent = "Player 1 Win ")
